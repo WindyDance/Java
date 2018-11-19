452adc59a20f8761f99211edac7aa67b0514c7d7
WindyDance <44084680+WindyDance@users.noreply.github.com>
2018-11-19 15:51:28 +0800

    Add files via upload

---
+++ generatorRunningClass.java
@@ -0,0 +1,26 @@
+package Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+
+public class generatorRunningClass {
+//mybatis逆向工程执行函数
+	public static void main(String[] args) throws Exception{
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		org.mybatis.generator.config.Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+		System.out.println("逆向完成");
+
+	}
+}
